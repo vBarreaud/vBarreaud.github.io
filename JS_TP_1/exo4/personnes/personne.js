7d2a6a3716aa056a117cbb91d0eef590256a70dc
VincentBarreaud <vincent.barreaud@enssat.fr>
2021-01-25 17:40:44 +0100

    version 2021 finalisée

---
+++ JS_TP_1/exo4/personnes/personne.js
@@ -0,0 +1,12 @@
+var personne = {
+  nom: ['Jean', 'Martin'],
+  age: 32,
+  sexe: 'masculin',
+  interets: ['musique', 'skier'],
+  bio: function() {
+    alert(this.nom[0] + ' ' + this.nom[1] + ' a ' + this.age + ' ans. Il aime ' + this.interets[0] + ' et ' + this.interets[1] + '.');
+  },
+  salutation: function() {
+    alert('Bonjour ! Je suis ' + this.nom[0] + '.');
+  }
+};
---
+++ JS_TP_1/exo4/voitures/voiture.js
@@ -0,0 +1,5 @@
+function Voiture(fabricant, modèle, année) {
+  this.fabricant = fabricant;
+  this.modele = modele;
+  this.annee = annee;
+}
