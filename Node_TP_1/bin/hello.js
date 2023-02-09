5075984e9ebc8917223cf4f10255bf2bd921c12e
Vincent BARREAUD <barreaud@enssat.fr>
2023-02-09 14:25:09 +0100

    First Node Lab

---
+++ Node_TP_1/bin/hello.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const chalk = require("chalk");
+const boxen = require("boxen");
+const yargs = require("yargs");
+const axios = require("axios");
+
+const options = yargs
+ .usage("Usage: -n <name>")
+ .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
+ .argv;
+
+const greeting = chalk.white.bold(`Hello, ${options.name}!`);
+
+const boxenOptions = {
+ padding: 1,
+ margin: 1,
+ borderStyle: "round",
+ borderColor: "green",
+ backgroundColor: "#555555"
+};
+const msgBox = boxen( greeting, boxenOptions );
+
+console.log(msgBox);
