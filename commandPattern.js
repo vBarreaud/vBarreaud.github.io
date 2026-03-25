51aa1c2a23069db6c446ec1f8c395a6291fcf790
Vincent Barreaud <vBarreaud@users.noreply.github.com>
2026-03-25 10:09:52 +0100

    added command Pattern

---
+++ commandPattern.js
@@ -0,0 +1,67 @@
+class OrderManager {
+  constructor() {
+    this.orders = []
+  }
+
+  placeOrder(order, id) {
+    this.orders.push(id)
+    return `You have successfully ordered ${order} (${id})`;
+  }
+
+  trackOrder(id) {
+    return `Your order ${id} will arrive in 20 minutes.`
+  }
+
+  cancelOrder(id) {
+    this.orders = this.orders.filter(order => order.id !== id)
+    return `You have canceled your order ${id}`
+  }
+}
+
+const manager = new OrderManager();
+
+console.log(manager.placeOrder("Pad Thai", "1234"))
+console.log(manager.trackOrder("1234"))
+console.log(manager.cancelOrder("1234"))
+
+
+class OrderManager_2 {
+  constructor() {
+    this.orders = [];
+  }
+
+  execute(command, ...args) {
+    return command.execute(this.orders, ...args);
+  }
+}
+
+class Command {
+  constructor(execute) {
+    this.execute = execute;
+  }
+}
+
+function PlaceOrderCommand(order, id) {
+  return new Command((orders) => {
+    orders.push(id);
+    return `You have successfully ordered ${order} (${id})`;
+  });
+}
+
+function CancelOrderCommand(id) {
+  return new Command((orders) => {
+    orders = orders.filter((order) => order.id !== id);
+    return `You have canceled your order ${id}`;
+  });
+}
+
+function TrackOrderCommand(id) {
+  return new Command(() => `Your order ${id} will arrive in 20 minutes.`);
+}
+
+
+const manager_2 = new OrderManager_2();
+
+console.log(manager_2.execute(new PlaceOrderCommand("Pad Thai", "1234")))
+console.log(manager_2.execute(new TrackOrderCommand("1234")))
+console.log(manager_2.execute(new CancelOrderCommand("1234")))
